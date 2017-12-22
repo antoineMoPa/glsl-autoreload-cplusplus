5a580461d7684633047f4a2fdb271e04a91f7cff
Antoine <antoine.morin.paulhus@gmail.com>
2017-12-22 00:59:31 -0500

    less intense default shader

--- fragment.glsl
+++ fragment.glsl
@@ -16,11 +16,15 @@ void main(){
 
 	vec2 p = UV - 0.5;
 	
-	float r = 0.0;
-	float g = 0.3 * cos(time * 30.0);
-	float b = 0.0;
+	float r = 0.1 + p.y;
+	float g = 0.3;
+	float b = 0.5;
 
-	if(p.y < 0.0 + 0.1 * cos(p.x * 10.0 + time * 6.28)){
+	float f = 0.003 * cos(p.x * 30.0 + time * 6.28);
+	f += 0.02 * cos(p.x * 4.0 + time * 6.28);
+	f += 0.01 * cos(p.x * 8.0 + time * 6.28);
+	
+	if(p.y < f){
 		r += 1.0;
 	}
 	
